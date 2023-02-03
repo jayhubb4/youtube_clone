2667444d0620aaa31c7aaa9dd01b20488e95ef67
Jonathan Hubbard <gsusplaya@gmail.com>
2023-02-02 22:57:30 -0500

    Preparing to import videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { Sidebar, Videos } from './components';
 
 const Feed = () => {
   return (
-    <div>
-      
-    </div>
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }} >
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2}}}>
+        Sidebar
+
+        <Typography 
+          className='copyright'
+          variant='body2'
+          sx={{ mt: 1.5, color: '#fff' }}
+        >
+          <Sidebar />
+          <br />
+          Copyright 2022 .com Media
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography 
+          variant='h4' 
+          fontWeight='bold' 
+          mb={2} 
+          sx={{ color: 'white'}}
+        >
+          New <span style={{ color: '#FC1503'}}>videos</span>
+        </Typography>
+
+        <Videos />
+      </Box>
+    </Stack>
   )
 }
 
